00d59244f1a038f2874ae86e1d1d49d49f30286a
Fredrik Makila <fredrik@takanoha.io>
2021-06-06 20:36:41 +0300

    Properly configure types

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="jest" />
+
+declare namespace jest {
+  export interface Matchers<R> {
+    toHaveEmitted(event: string, payload?: any): R;
+    toHaveEmittedPayload(event: string, payload: any): R;
+  }
+}
